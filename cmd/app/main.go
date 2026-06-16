c3560c32e79a0f36d5fc6a2dd22b8dc8dd11807b
RupenderSinghRathore <kamisamakirishima@gmail.com>
2026-06-16 06:43:14 +0530

    chore: create initial project structure

---
+++ cmd/app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+const AppName = "authcli"
+
+func main() {
+	fmt.Printf("Wellcome to %s\n", AppName)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module RupenderSinghRathore/AuthCli
+
+go 1.26.4
